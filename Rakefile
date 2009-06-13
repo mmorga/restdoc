0dd419a8fd6b0dde5be5551ad15f3a3af2f92802
Mark Morga <mmorga@mark-morgas-mac-pro.local>
2009-06-13 12:07:33 -0500

    saving my changes

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/lib/restdoc'
 
 # Generate all the Rake tasks
 # Run 'rake -T' to see list of generated tasks (from gem root directory)
-$hoe = Hoe.new('restdoc', Restdoc::VERSION) do |p|
+$hoe = Hoe.new('restdoc', RestDoc::VERSION) do |p|
   p.developer('mmorga', 'markmorga@gmail.com')
   p.changes              = p.paragraphs_of("History.txt", 0..1).join("\n\n")
   p.rubyforge_name       = p.name # TODO this is default value
--- lib/restdoc/restdoc/restdoc.rb
+++ lib/restdoc/restdoc/restdoc.rb
@@ -4,6 +4,42 @@ require 'rdoc/options'
 require 'rdoc/generators/html_generator'
 
 class RestDoc::RestDoc
+
+  def template_exists_for(controller, action)
+    not Dir.glob(File.join(RAILS_ROOT, "app", "views", controller, "#{action}.*")).empty?
+  end
+
+  def process_route(route)
+    controller_class = (route.defaults[:controller] + "Controller").camelize
+    return nil if controller_class.constantize.new.respond_to?(route.defaults[:action])
+    return nil if template_exists_for(route.defaults[:controller], route.defaults[:action])
+
+    "#{controller_class.to_s} does not respond to '#{route.defaults[:action]}' as needed for #{route.defaults.inspect}"
+  rescue NameError => error
+    "#{controller_class.to_s} does not exist, it is needed for #{route.defaults.inspect}"
+  end
+
+    problems = {}
+
+    ActionController::Routing::Routes.routes.each do |route|
+      if route.defaults.has_key?(:controller) && route.defaults.has_key?(:action)
+
+        result = process_route(route)
+
+        if result
+          problems[route.defaults[:controller]] ||= []
+          problems[route.defaults[:controller]] << result
+        end
+      end
+    end
+
+    unless problems.empty?
+      puts "***** Routes which correspond to missing actions on controller methods *****"
+      problems.each { |key, problem| puts problem.uniq.sort }
+      puts "***** PLEASE FIX THESE ROUTES BY MAY 15 2009 *****"
+    end
+  end
+
   def document(args)
     fn = "something.rb"
     top_level = RDoc::TopLevel.new(fn)
